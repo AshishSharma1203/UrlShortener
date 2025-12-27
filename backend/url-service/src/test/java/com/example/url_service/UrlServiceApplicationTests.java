60de06a26f0c5270a2e64c88bf7451e8b1373fe5
Ashish <akrsharma766@gmail.com>
2025-12-28 02:15:15 +0530

    initial setup of the url service

---
+++ backend/url-service/src/test/java/com/example/url_service/UrlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.url_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
