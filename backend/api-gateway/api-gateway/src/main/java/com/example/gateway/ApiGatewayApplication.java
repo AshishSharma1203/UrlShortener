e40ed4f99b04a9f1422cfff688903e4d8174c206
Ashish <akrsharma766@gmail.com>
2025-12-27 17:47:15 +0530

    Update .gitignore with comprehensive rules

---
+++ backend/api-gateway/api-gateway/src/main/java/com/example/gateway/ApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+        System.out.println("API Gateway Service is up & running ");
+	}
+
+}
